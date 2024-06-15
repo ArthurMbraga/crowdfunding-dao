7716f54ff2f1cf3213e5a38a173fc948d100866a
yurialvizi <yurialvizi@gmail.com>
2024-06-15 18:03:17 +0200

    add deployment by metamask wallet

--- DAO/scripts/deploy.js
+++ DAO/scripts/deploy.js
@@ -1,22 +1,37 @@
+require("dotenv").config();
 const { ethers } = require("ethers");
 const fs = require("fs");
 const path = require("path");
 
 async function deployDAO() {
     const provider = new ethers.JsonRpcProvider();
-    const signer = await provider.getSigner();
+    const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
+    if (!deployerPrivateKey) {
+        throw new Error("Deployer private key is not defined in .env file");
+    }
+
+    const deployer = new ethers.Wallet(deployerPrivateKey, provider);
+
+    console.log("Deploying contracts with the account:", deployer.getAddress());
+    console.log("Account balance:", (await provider.getBalance(deployer.getAddress())).toString());
 
     const daoArtifact = JSON.parse(fs.readFileSync(path.join(__dirname, "../artifacts/contracts/DAO.sol/DAO.json"), "utf8"));
     const daoAbi = daoArtifact.abi;
     const daoBytecode = daoArtifact.bytecode;
 
-    const daoFactory = new ethers.ContractFactory(daoAbi, daoBytecode, signer);
+    const daoFactory = new ethers.ContractFactory(daoAbi, daoBytecode, deployer);
 
-    const initialRecipients = [
-        process.env.WALLET_ADDRESS1,
-        process.env.WALLET_ADDRESS2,
-        process.env.WALLET_ADDRESS3,
-    ];
+    const initialRecipients = [];
+    for (let i = 1; i <= 3; i++) {
+        const address = process.env[`WALLET_ADDRESS${i}`];
+        if (!address) {
+            throw new Error(`WALLET_ADDRESS${i} is not defined in .env file`);
+        }
+        initialRecipients.push(address);
+    }
+
+    console.log("Initial recipients:", initialRecipients);
+
     const initialAmounts = [
         ethers.parseUnits("1000", 18),
         ethers.parseUnits("1000", 18),
