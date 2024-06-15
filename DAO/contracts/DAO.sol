96c4fbcc849e8c86012f348c7787d2ba5dcda9e5
yurialvizi <yurialvizi@gmail.com>
2024-06-15 18:26:27 +0200

    feat: Add rejection functionality for expired campaign proposals

--- DAO/contracts/DAO.sol
+++ DAO/contracts/DAO.sol
@@ -11,16 +11,19 @@ contract DAO {
         address campaign;
         uint256 approvals;
         mapping(address => bool) approvedBy;
+        uint256 creationTime;
     }
 
     mapping(address => ProposedCampaign) private proposedCampaigns;
     uint256 public totalManagerTokens;
 
-    address[] private proposedCampaigns;
+    address[] private proposedCampaignsList;
     address[] private listedCampaigns;
+    address[] private rejectedCampaigns;
 
     event CampaignCreated(address indexed campaignAddress, address indexed creator, uint256 goal, uint256 deadline);
     event CampaignApproved(address indexed campaignAddress, address indexed approver, uint256 approvals);
+    event CampaignRejected(address indexed campaignAddress);
 
     constructor(address[] memory initialRecipients, uint256[] memory initialAmounts) {
         require(initialRecipients.length == initialAmounts.length, "Recipients and amounts length mismatch");
@@ -39,7 +42,8 @@ contract DAO {
 
         ProposedCampaign storage proposal = proposedCampaigns[campaignAddress];
         proposal.campaign = campaignAddress;
-        proposedCampaigns.push(campaignAddress);
+        proposal.creationTime = block.timestamp;
+        proposedCampaignsList.push(campaignAddress);
 
         emit CampaignCreated(campaignAddress, msg.sender, goal, deadline);
 
@@ -48,6 +52,8 @@ contract DAO {
 
     function approveCampaign(address campaignAddress) external {
         require(managerToken.balanceOf(msg.sender) > 0, "Only managers can approve campaigns");
+        checkAndRejectExpiredProposals();
+        require(isInProposedCampaignsList(campaignAddress), "Campaign is not in proposed campaigns list");
 
         ProposedCampaign storage proposal = proposedCampaigns[campaignAddress];
         require(proposal.campaign != address(0), "Campaign does not exist");
@@ -79,16 +85,43 @@ contract DAO {
     }
 
     function getProposedCampaigns() public view returns (address[] memory) {
-        return proposedCampaigns;
+        return proposedCampaignsList;
+    }
+
+    function getRejectedCampaigns() public view returns (address[] memory) {
+        return rejectedCampaigns;
     }
 
     function removeProposedCampaign(address campaignAddress) internal {
-        for (uint256 i = 0; i < proposedCampaigns.length; i++) {
-            if (proposedCampaigns[i] == campaignAddress) {
-                proposedCampaigns[i] = proposedCampaigns[proposedCampaigns.length - 1];
-                proposedCampaigns.pop();
+        for (uint256 i = 0; i < proposedCampaignsList.length; i++) {
+            if (proposedCampaignsList[i] == campaignAddress) {
+                proposedCampaignsList[i] = proposedCampaignsList[proposedCampaignsList.length - 1];
+                proposedCampaignsList.pop();
                 break;
             }
         }
+    }
+
+    function checkAndRejectExpiredProposals() external {
+        uint256 currentTime = block.timestamp;
+        for (uint256 i = 0; i < proposedCampaignsList.length; i++) {
+            address campaignAddress = proposedCampaignsList[i];
+            ProposedCampaign storage proposal = proposedCampaigns[campaignAddress];
+            if (currentTime > proposal.creationTime + 10 minutes) {
+                rejectedCampaigns.push(campaignAddress);
+                removeProposedCampaign(campaignAddress);
+                emit CampaignRejected(campaignAddress);
+                i--; // Adjust index after removal
+            }
+        }
+    }
+
+    function isInProposedCampaignsList(address campaignAddress) internal view returns (bool) {
+        for (uint256 i = 0; i < proposedCampaignsList.length; i++) {
+            if (proposedCampaignsList[i] == campaignAddress) {
+                return true;
+            }
+        }
+        return false;
     }
 }
